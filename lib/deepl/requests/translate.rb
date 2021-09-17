7d7ced965e88739b49ee8ddc88aa66113a97a668
Auvi R <getauvi23@gmail.com>
2021-09-17 17:07:04 +0200

    Add glossary_id to API calls

--- lib/deepl/requests/translate.rb
+++ lib/deepl/requests/translate.rb
@@ -5,26 +5,31 @@ module DeepL
     class Translate < Base
       BOOLEAN_CONVERSION = { true => '1', false => '0' }.freeze
       ARRAY_CONVERSION = ->(value) { value.is_a?(Array) ? value.join(', ') : value }.freeze
+      NO_CONVERSION = ->(value) { value }
       OPTIONS_CONVERSIONS = {
         split_sentences: BOOLEAN_CONVERSION,
         preserve_formatting: BOOLEAN_CONVERSION,
         non_splitting_tags: ARRAY_CONVERSION,
-        ignore_tags: ARRAY_CONVERSION
+        ignore_tags: ARRAY_CONVERSION,
+        glossary_id: NO_CONVERSION,
       }.freeze
 
-      attr_reader :text, :source_lang, :target_lang, :ignore_tags, :non_splitting_tags
+      attr_reader :text, :source_lang, :target_lang, :ignore_tags, :non_splitting_tags, :glossary_id
 
       def initialize(api, text, source_lang, target_lang, options = {})
         super(api, options)
         @text = text
         @source_lang = source_lang
         @target_lang = target_lang
+        @glossary_id = options[:glossary_id]
 
         tweak_parameters!
       end
 
       def request
         payload = { text: text, source_lang: source_lang, target_lang: target_lang }
+        payload.merge!({ glossary_id: glossary_id }) if glossary_id.present?
+
         build_texts(*post(payload))
       end
 
